00f8e9348c998591e261501d6fb8678772756e88
unknown <20203tn077@utez.edu.mx>
2022-03-17 11:55:46 -0600

    Clase 17/03/2022 2

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,7 +1,45 @@
 import React from 'react'
+import * as yup from 'yup'
+import FeatherIcon from 'feather-icons-react'
+import img from '../../assets/img/marketplace.png'
+import { useNavigate } from 'react-router-dom'
+import { useFormik } from 'formik'
+import axios from 'axios'
+
+
 
 export const LoginScreen = () => {
+  const navigation = useNavigate()
+  const {dispatch } = 
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: ''
+    },
+    validationSchema: yup.object().shape({
+      username: yup.string.required('campo obligatorio'),
+      password: yup.string.required('campo obligatorio')
+
+    }),
+    onsubmit: (values) => {
+      axios({
+        url: '/auth/login',
+        method: 'POST',
+        data: JSON.stringify(values)
+      }).then((response.error)) {
+        const action = {
+          type: 'LOGIN',
+          payload: response.data
+        };
+        dispatch(action)
+        navigation('products', {replace: true})
+      }
+    }
+  })
+
   return (
-    <div>LoginScreen</div>
+    <>
+
+    </>
   )
 }
